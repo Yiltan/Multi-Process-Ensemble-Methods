31743551b6d2564a6cc73a91826b5c1c58ec03c2
Yiltan <yiltan.github@gmail.com>
2020-03-04 12:36:31 -0500

    added reader for annotations

---
+++ src/preprocess.py
@@ -0,0 +1,6 @@
+import reader
+
+# Used for testing
+if __name__ == "__main__":
+    df = reader.get_annotations(1, 18)
+    print(df)
--- src/reader.py
+++ src/reader.py
@@ -1,6 +1,10 @@
+import pandas as pd
 from scipy.io import loadmat
 
-def get_matlab_data(signal_type, participant_id, video_id):
+_data_dir = "./data"
+
+def get_matlab_data(signal_type, user_id, video_id):
+    # Do we want to do Preprossing here or in another function?
     data = loadmat('data/Data_Original_P01.mat')
 
     # Sensor Data
@@ -10,3 +14,29 @@ def get_matlab_data(signal_type, participant_id, video_id):
 
     # Other
     video_ids = data['VideoIDs']
+
+def get_annotations(user_id, video_number):
+
+    valid_inputs = 0 < user_id < 41 and 0 < video_number < 21
+
+    if valid_inputs:
+        df = pd.read_excel(_data_dir + '/External_Annotations.xlsx')
+
+        # Filter
+        df = df[df['UserID'] == user_id]
+        df = df[df['Video_Number'] == video_number]
+
+        # Clean
+        df = df.reset_index()
+        df = df.drop(['index', 'UserID', 'Video_Number', 'VideoID'], axis=1)
+
+        return df
+
+    raise Exception("Invalid Inputs")
+
+# Used for testing
+if __name__ == "__main__":
+    df = get_annotations(5, 18)
+    print(df)
+    df = get_annotations(50, 18)
+

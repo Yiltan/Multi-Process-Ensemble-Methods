77b47f01dc269ee21eefc26def2ca673285ff589
Yiltan <yiltan.github@gmail.com>
2020-03-04 14:22:49 -0500

    added cache so we do not read every time

--- src/reader.py
+++ src/reader.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from scipy.io import loadmat
 
+_annotations_cache = None
+
 _data_dir = "./data"
+_annotation_path = _data_dir + '/External_Annotations.xlsx'
 
 def get_matlab_data(signal_type, user_id, video_id):
     # Do we want to do Preprossing here or in another function?
@@ -20,7 +23,11 @@ def get_annotations(user_id, video_number):
     valid_inputs = 0 < user_id < 41 and 0 < video_number < 21
 
     if valid_inputs:
-        df = pd.read_excel(_data_dir + '/External_Annotations.xlsx')
+        global _annotations_cache
+        if _annotations_cache is None:
+            _annotations_cache = pd.read_excel(_annotation_path)
+
+        df = _annotations_cache
 
         # Filter
         df = df[df['UserID'] == user_id]

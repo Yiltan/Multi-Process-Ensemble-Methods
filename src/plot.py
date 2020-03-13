be06e3a12f3df871c24ce5d532b2a3721b386a61
Yiltan <yiltan.github@gmail.com>
2020-03-13 12:38:26 -0400

    added preprocessing

--- src/plot.py
+++ src/plot.py
@@ -1,33 +1,33 @@
-import reader
+import preprocess
 import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('text', usetex=True)
 plt.figure(figsize=(8, 4))
 
-column_name = { 4 : "AF3",
-                5 : "F7",
-                6 : "F3",
-                7 : "FC5",
-                8 : "T7",
-                9 : "P7",
-               10 : "O1",
-               11 : "O2",
-               12 : "P8",
-               13 : "T8",
-               14 : "FC6",
-               15 : "F4",
-               16 : "F8",
-               17 : "AF4"}
+column_name = { 3 : "AF3",
+                4 : "F7",
+                5 : "F3",
+                6 : "FC5",
+                7 : "T7",
+                8 : "P7",
+                9 : "O1",
+               10 : "O2",
+               11 : "P8",
+               12 : "T8",
+               13 : "FC6",
+               14 : "F4",
+               15 : "F8",
+               16 : "AF4"}
 
 if __name__ == "__main__":
-    data = reader.get_matlab_data('eeg', 1, 12)
+    data = preprocess.get_processed_data('eeg', 1, 12)
 
     # Time stamp is column 20
     x = data[20]
     x = x - x[0] # Start time from zero
 
-    # EEG channels are 4-17
-    for channel in range(4, 18):
+    # EEG channels are 3-16
+    for channel in range(3, 17):
         y = data[channel]
 
         plt.plot(x, y)
---
+++ src/preprocess.py
@@ -0,0 +1,36 @@
+import reader
+import filters
+import time
+
+def get_processed_data(signal_type, user_id, video_number):
+    raw_data = reader.get_matlab_data(signal_type, user_id, video_number)
+
+    # Each signal is filtered differently
+    # We are following the guidelines set here:
+    # http://www.eecs.qmul.ac.uk/mmv/datasets/amigos/readme.html#prep
+    if signal_type == 'eeg':
+        return filter_eeg(raw_data)
+    elif signal_type == 'ecg':
+        return filter_ecg(raw_data)
+    elif signal_type == 'gsr':
+        return raw_data
+    else:
+        raise Exception("Invalid data type")
+    # Add SVM etc inside here
+
+def filter_eeg(data):
+    # EEG channels are 3-16
+    for channel in range(3, 17):
+        data[channel] = filters.band_pass(4, 45, 128, data[channel])
+    return data
+
+def filter_ecg(data):
+    # ECG channels are 1 & 2
+    for channel in [1, 2]:
+        data[channel] = filters.low_pass(60 , 128, data[channel])
+    return data
+
+# Used for testing
+if __name__ == "__main__":
+    print(get_processed_data('eeg', 1, 12))
+    print(get_processed_data('ecg', 1, 12))

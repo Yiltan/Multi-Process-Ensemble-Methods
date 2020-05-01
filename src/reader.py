fd84bcb705dba646768a92dba2d3778060d12f34
amirsojoodi <amir.sojoodi@gmail.com>
2020-05-01 06:28:21 -0400

    Dev: add dimension reduction with PCA. No. of dimension goes from 384 to 40

--- src/reader.py
+++ src/reader.py
@@ -5,11 +5,15 @@ _number_of_users = 40
 _videos_per_user = 16 
 # 17-20 correspond to the long videos experiment
 _missing_data_subject = [9, 12, 21, 22, 23, 24, 33]
+
 _annotations_cache = None
+_assessments_cache = None
 _matlab_data_cache = dict()
 
 _data_dir = "./data"
-_annotation_path = _data_dir + '/External_Annotations.xlsx'
+_annotations_path = _data_dir + '/External_Annotations.xlsx'
+_assessments_path = _data_dir + '/SelfAsessment.xlsx'
+_features_path = _data_dir + '/features-total.csv'
 
 def get_matlab_data(signal_type, user_id, video_number):
 	
@@ -67,18 +71,65 @@ def get_annotations(user_id, video_number):
 	df = df.reset_index()
 	df = df.drop(['index', 'UserID', 'Video_Number', 'VideoID'], axis=1)
 	return df
+	
+# video_number starts from 0 to 15 for short movies
+def get_assessments(user_id, video_number):
+	global _number_of_users, _videos_per_user
+	valid_inputs = 0 < user_id <= _number_of_users and 0 <= video_number < _videos_per_user
+
+	assert valid_inputs, "Invalid Inputs!"
+
+	global _assessments_cache
+	if _assessments_cache is None:
+		_assessments_cache = pd.read_excel(_assessments_path)
+	_assessments_cache = _assessments_cache.drop(['Rep_Index', 'arousal', 'valence', 'dominance', 'liking', 'familiarity',
+					'familiarity.1', 'arousal.1', 'valence.1', 'dominance.1', 'liking.1'], axis=1)
+	
+	df = _assessments_cache
+	# Filter
+	df = df[df['UserID'] == user_id]
+	df = df[df['Video_Number'] == video_number]
+	
+	# Clean
+	df = df
+	df = df.reset_index()
+	return df
+
+def get_all_assessments():
+	# make sure get_assessments() runs at least once
+	get_assessments(5,5)
+	return _assessments_cache
+	
+def get_features():
+	df = pd.read_csv(_features_path, header=None)
+	# user_video = df.loc[:, [0,1]].values
+	# df = df.drop([0, 1], axis=1)
+	return df
 
 # Used for testing
 if __name__ == "__main__":
 	# Get data of a specific user and a specific video
-	df = get_annotations(5, 18)
-	#print(df)
+	# df = get_annotations(5, 18)
+	# print(df)
 	
 	# This line should raise the Exception
 	# df = get_annotations(50, 18)
-	data = get_matlab_data('eeg', 5, 1)
+	# data = get_matlab_data('eeg', 5, 1)
+	# print(data.shape)
+	# data = get_matlab_data('ecg', 10, 2)
+	# print(data.shape)
+	# data = get_matlab_data('gsr', 10, 3)
+	# print(data.shape)
+	
+	df = get_features()
+	# print(user_video)
+	# print(user_video.shape)
+	# print(df)
+	print(df.shape)
+	
+	# data = get_assessments(1, 6)
+	# print(data.head())
+	
+	data = get_all_assessments()
 	print(data.shape)
-	data = get_matlab_data('ecg', 10, 2)
-	print(data.shape)
-	data = get_matlab_data('gsr', 10, 3)
-	print(data.shape)
+	print(data.head())

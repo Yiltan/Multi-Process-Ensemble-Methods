97c320f2c50295b7cbd425df2d468e99f6e80389
amirsojoodi <amir.sojoodi@gmail.com>
2020-05-01 17:31:56 -0400

    Dev: add ensemble model, voting classifier

--- src/learning.py
+++ src/learning.py
@@ -9,11 +9,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.ensemble import VotingClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
 
 def logist_regression(categories):
 	
-	print("\n---------- Logistic Regression -----------")
+	#print("\n---------- Logistic Regression -----------")
 	
 	f1_scores = {}
 	accuracies = {}
@@ -46,7 +48,7 @@ def logist_regression(categories):
 
 def linear_svm(categories):
 	
-	print("\n---------- Linear SVM -----------")
+	#print("\n---------- Linear SVM -----------")
 	
 	f1_scores = {}
 	accuracies = {}
@@ -105,7 +107,7 @@ def knn(categories, k_range):
 			cnfsn_matrix = confusion_matrix(test_label, predicted)
 			#print("f1_score: {0}".format(f_one_score))
 			#print("Accuracy: {0}".format(accuracy))
-			# print(cnfsn_matrix)
+			#print(cnfsn_matrix)
 			#sys.stdout.flush()
 			
 			f1_scores[category] = f_one_score
@@ -137,22 +139,97 @@ def knn(categories, k_range):
 	#print(best_Ks)
 	return max_f1_scores, max_accuracies, cnfsn_matrices, best_Ks
 
+
+def gaussian_nb(categories, KNN_best_Ks):
+	
+	# print("\n---------- Gaussian -----------")
+	
+	f1_scores = {}
+	accuracies = {}
+	cnfsn_matrices = {}
+	
+	for category in categories:
+		#print("\n------ Category: " + category)
+		labels, data = dr.reduce_dimensions(dimensions=40, category=category)
+	
+		train_data, test_data, train_label, test_label = train_test_split(
+			data, labels, test_size=0.15, random_state=0)
+
+		svc = svm.LinearSVC(dual=False, max_iter=10000,probability=True)	
+		knn = KNeighborsClassifier(n_neighbors=KNN_best_Ks[category])	
+		logistic_regr = LogisticRegression(max_iter=10000)
+
+		estimators = [('knn', knn), ('svc', svc), ('log_reg', logistic_regr)]
+		ensemble = VotingClassifier(estimators, voting='soft', weights=[2,1,1])
+		ensemble.fit(train_data, train_label)
+		ensemble_predicted = ensemble.predict(test_data)
+		
+		f_one_score = f1_score(test_label, ensemble_predicted, average='weighted')
+		accuracy = metrics.accuracy_score(test_label, ensemble_predicted)
+		cnfsn_matrix = confusion_matrix(test_label, ensemble_predicted)
+		#print("f1_score: {0}".format(f_one_score))
+		#print("Accuracy: {0}".format(accuracy))
+		#print(cnfsn_matrix)
+		#sys.stdout.flush()
+		
+		f1_scores[category] = f_one_score
+		accuracies[category] = accuracy
+		cnfsn_matrices[category] = cnfsn_matrix
+	
+	return f1_scores, accuracies, cnfsn_matrices
+	
+def ensemble(categories, KNN_best_Ks):
+	
+	# print("\n---------- Ensemble -----------")
+	
+	f1_scores = {}
+	accuracies = {}
+	cnfsn_matrices = {}
+	
+	for category in categories:
+		#print("\n------ Category: " + category)
+		labels, data = dr.reduce_dimensions(dimensions=40, category=category)
+	
+		train_data, test_data, train_label, test_label = train_test_split(
+			data, labels, test_size=0.15, random_state=0)
+
+		svc = svm.LinearSVC(dual=False, max_iter=10000)	
+		knn = KNeighborsClassifier(n_neighbors=KNN_best_Ks[category])	
+		logistic_regr = LogisticRegression(max_iter=10000)
+
+		estimators = [('knn', knn), ('svc', svc), ('log_reg', logistic_regr)]
+		ensemble = VotingClassifier(estimators, voting='hard', weights=[2,1,1])
+		ensemble.fit(train_data, train_label)
+		ensemble_predicted = ensemble.predict(test_data)
+		
+		f_one_score = f1_score(test_label, ensemble_predicted, average='weighted')
+		accuracy = metrics.accuracy_score(test_label, ensemble_predicted)
+		cnfsn_matrix = confusion_matrix(test_label, ensemble_predicted)
+		#print("f1_score: {0}".format(f_one_score))
+		#print("Accuracy: {0}".format(accuracy))
+		#print(cnfsn_matrix)
+		#sys.stdout.flush()
+		
+		f1_scores[category] = f_one_score
+		accuracies[category] = accuracy
+		cnfsn_matrices[category] = cnfsn_matrix
+	
+	return f1_scores, accuracies, cnfsn_matrices
 # Used for testing
 if __name__ == "__main__":
 	categories = ['neutral','disgust','happiness','surprise','anger','fear','sadness']
 	
 	LR_f1, LR_accuracy, LR_cnfsn = logist_regression(categories)
 	SVM_f1, SVM_accuracy, SVM_cnfsn = linear_svm(categories)
-	KNN_f1, KNN_accuracy, KNN_cnfsn, KNN_best_Ks = knn(categories, range(1, 26))
+	KNN_f1, KNN_accuracy, KNN_cnfsn, KNN_best_Ks = knn(categories, range(1, 9)) #(1,25)
+	ENS_f1, ENS_accuracy, ENS_cnfsn = ensemble(categories, KNN_best_Ks)
 	
 	# You can print the scores right away, like:
 	# print(LR_f1)
 	# print(LR_accuracy)
 	
-	print(KNN_f1)
-	
 	for category in categories:
-		print("\n------ Category: {0} -------\n".format(category))
+		print("\n------ Category: {0} -------".format(category))
 		print("\n1. Logistic Regression: ")
 		print("\tf1_score: {0}".format(LR_f1[category]))
 		print("\tAccuracy: {0}".format(LR_accuracy[category]))
@@ -165,6 +242,10 @@ if __name__ == "__main__":
 		print("\tf1_score: {0}".format(KNN_f1[category]))
 		print("\tAccuracy: {0}".format(KNN_accuracy[category]))
 		print("\tBest K: {0}".format(KNN_best_Ks[category]))
+		
+		print("\n4. ENS: ")
+		print("\tf1_score: {0}".format(ENS_f1[category]))
+		print("\tAccuracy: {0}".format(ENS_accuracy[category]))
 		
 		sys.stdout.flush()
 	

d131e5cbf45a600b16a685ab391dee83dbe75716
amirsojoodi <amir.sojoodi@gmail.com>
2020-05-01 18:22:26 -0400

    Dev: add AdaBoost model

--- src/learning.py
+++ src/learning.py
@@ -12,26 +12,28 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import AdaBoostClassifier
 
 def logist_regression(categories):
-
+	
 	#print("\n---------- Logistic Regression -----------")
-
+	
 	f1_scores = {}
 	accuracies = {}
 	cnfsn_matrices = {}
-
+	
 	for category in categories:
 		#print("\n------ Category: " + category)
 		labels, data = dr.reduce_dimensions(dimensions=40, category=category)
-
+	
 		train_data, test_data, train_label, test_label = train_test_split(
 			data, labels, test_size=0.15, random_state=0)
 
 		logistic_regr = LogisticRegression(max_iter=10000)
 		logistic_regr.fit(train_data, train_label)
 		predicted = logistic_regr.predict(test_data)
-
+		
 		f_one_score = f1_score(test_label, predicted, average='weighted')
 		accuracy = metrics.accuracy_score(test_label, predicted)
 		cnfsn_matrix = confusion_matrix(test_label, predicted)
@@ -39,32 +41,32 @@ def logist_regression(categories):
 		#print("Accuracy: {0}".format(accuracy))
 		#print(cnfsn_matrix)
 		#sys.stdout.flush()
-
+		
 		f1_scores[category] = f_one_score
 		accuracies[category] = accuracy
 		cnfsn_matrices[category] = cnfsn_matrix
-
+	
 	return f1_scores, accuracies, cnfsn_matrices
 
 def linear_svm(categories):
-
+	
 	#print("\n---------- Linear SVM -----------")
-
+	
 	f1_scores = {}
 	accuracies = {}
 	cnfsn_matrices = {}
-
+	
 	for category in categories:
 		#print("\n------ Category: " + category)
 		labels, data = dr.reduce_dimensions(dimensions=40, category=category)
-
+	
 		train_data, test_data, train_label, test_label = train_test_split(
 			data, labels, test_size=0.15, random_state=0)
 
 		svc = svm.LinearSVC(dual=False, max_iter=10000)
 		svc.fit(train_data, train_label)
 		predicted = svc.predict(test_data)
-
+		
 		f_one_score = f1_score(test_label, predicted, average='weighted')
 		accuracy = metrics.accuracy_score(test_label, predicted)
 		cnfsn_matrix = confusion_matrix(test_label, predicted)
@@ -72,11 +74,11 @@ def linear_svm(categories):
 		#print("Accuracy: {0}".format(accuracy))
 		#print(cnfsn_matrix)
 		#sys.stdout.flush()
-
+		
 		f1_scores[category] = f_one_score
 		accuracies[category] = accuracy
 		cnfsn_matrices[category] = cnfsn_matrix
-
+	
 	return f1_scores, accuracies, cnfsn_matrices
 
 def knn(categories, k_range):
@@ -90,11 +92,11 @@ def knn(categories, k_range):
 		accuracies = {}
 		f1_scores = {}
 		cnfsn_matrices = {}
-
+		
 		for category in categories:
 			#print("\n------ Category: " + category)
 			labels, data = dr.reduce_dimensions(dimensions=40, category=category)
-
+		
 			train_data, test_data, train_label, test_label = train_test_split(
 				data, labels, test_size=0.15, random_state=0)
 
@@ -109,22 +111,22 @@ def knn(categories, k_range):
 			#print("Accuracy: {0}".format(accuracy))
 			#print(cnfsn_matrix)
 			#sys.stdout.flush()
-
+			
 			f1_scores[category] = f_one_score
 			accuracies[category] = accuracy
 			cnfsn_matrices[category] = cnfsn_matrix
-
+		
 		accuracies_list.append(accuracies)
 		f1_scores_list.append(f1_scores)
 		cnfsn_matrices_list.append(cnfsn_matrices)
-
+	
 	# Select the best k based on the f1 score
 	max_accuracies = {}
 	max_f1_scores = {}
 	cnfsn_matrices = {}
 	best_Ks = {}
 	for category in categories:
-
+		
 		max_accuracies[category] = 0
 		max_f1_scores[category] = 0
 		best_Ks[category] = 0
@@ -140,27 +142,30 @@ def knn(categories, k_range):
 	return max_f1_scores, max_accuracies, cnfsn_matrices, best_Ks
 
 
-def gaussian_nb(categories):
-
-	# print("\n---------- Gaussian -----------")
-
+def ada_boost_classifier(categories):
+	
+	# print("\n---------- AdaBoost Classifier -----------")
+	
 	f1_scores = {}
 	accuracies = {}
 	cnfsn_matrices = {}
-
+	
 	for category in categories:
 		#print("\n------ Category: " + category)
 		labels, data = dr.reduce_dimensions(dimensions=40, category=category)
-
+	
 		train_data, test_data, train_label, test_label = train_test_split(
 			data, labels, test_size=0.15, random_state=0)
 
-		kernel = 1.0 * RBF(1.0)
-
-		classifier = GaussianProcessClassifier(kernel=kernel, random_state=0)
-		classifier.fit(train_data, train_label)
-		classifier_predicted = classifier.predict(test_data)
-
+		#kernel = 1.0 * RBF(1.0)
+		#classifier = GaussianProcessClassifier(kernel=kernel, random_state=0)
+		#classifier.fit(train_data, train_label)
+		#classifier_predicted = classifier.predict(test_data)
+		
+		ada= AdaBoostClassifier(n_estimators=100)
+		ada.fit(train_data, train_label)
+		classifier_predicted = ada.predict(test_data)
+		
 		f_one_score = f1_score(test_label, classifier_predicted, average='weighted')
 		accuracy = metrics.accuracy_score(test_label, classifier_predicted)
 		cnfsn_matrix = confusion_matrix(test_label, classifier_predicted)
@@ -168,37 +173,37 @@ def gaussian_nb(categories):
 		#print("Accuracy: {0}".format(accuracy))
 		#print(cnfsn_matrix)
 		#sys.stdout.flush()
-
+		
 		f1_scores[category] = f_one_score
 		accuracies[category] = accuracy
 		cnfsn_matrices[category] = cnfsn_matrix
-
+	
 	return f1_scores, accuracies, cnfsn_matrices
-
+	
 def ensemble(categories, KNN_best_Ks):
-
+	
 	# print("\n---------- Ensemble -----------")
-
+	
 	f1_scores = {}
 	accuracies = {}
 	cnfsn_matrices = {}
-
+	
 	for category in categories:
 		#print("\n------ Category: " + category)
 		labels, data = dr.reduce_dimensions(dimensions=40, category=category)
-
+	
 		train_data, test_data, train_label, test_label = train_test_split(
 			data, labels, test_size=0.15, random_state=0)
 
-		svc = svm.LinearSVC(dual=False, max_iter=10000)
-		knn = KNeighborsClassifier(n_neighbors=KNN_best_Ks[category])
+		svc = svm.LinearSVC(dual=False, max_iter=10000)	
+		knn = KNeighborsClassifier(n_neighbors=KNN_best_Ks[category])	
 		logistic_regr = LogisticRegression(max_iter=10000)
 
 		estimators = [('knn', knn), ('svc', svc), ('log_reg', logistic_regr)]
 		ensemble = VotingClassifier(estimators, voting='hard', weights=[2,1,1])
 		ensemble.fit(train_data, train_label)
 		ensemble_predicted = ensemble.predict(test_data)
-
+		
 		f_one_score = f1_score(test_label, ensemble_predicted, average='weighted')
 		accuracy = metrics.accuracy_score(test_label, ensemble_predicted)
 		cnfsn_matrix = confusion_matrix(test_label, ensemble_predicted)
@@ -206,50 +211,49 @@ def ensemble(categories, KNN_best_Ks):
 		#print("Accuracy: {0}".format(accuracy))
 		#print(cnfsn_matrix)
 		#sys.stdout.flush()
-
+		
 		f1_scores[category] = f_one_score
 		accuracies[category] = accuracy
 		cnfsn_matrices[category] = cnfsn_matrix
-
+	
 	return f1_scores, accuracies, cnfsn_matrices
 # Used for testing
 if __name__ == "__main__":
 	categories = ['neutral','disgust','happiness','surprise','anger','fear','sadness']
-
+	
 	LR_f1, LR_accuracy, LR_cnfsn = logist_regression(categories)
-	#GNB_f1, GNB_accuracy, GNB_cnfsn = gaussian_nb(categories)
 	SVM_f1, SVM_accuracy, SVM_cnfsn = linear_svm(categories)
 	KNN_f1, KNN_accuracy, KNN_cnfsn, KNN_best_Ks = knn(categories, range(1, 9)) #(1,25)
+	ABC_f1, ABC_accuracy, ABC_cnfsn = ada_boost_classifier(categories)
 	ENS_f1, ENS_accuracy, ENS_cnfsn = ensemble(categories, KNN_best_Ks)
-
+	
 	# You can print the scores right away, like:
 	# print(LR_f1)
 	# print(LR_accuracy)
-
+	
 	for category in categories:
 		print("\n------ Category: {0} -------".format(category))
-		'''
-		print("\n0. Gaussian Naive Bayes: ")
-		print("\tf1_score: {0}".format(GNB_f1[category]))
-		print("\tAccuracy: {0}".format(GNB_accuracy[category]))
-		'''
 
-#		print("\n1. Logistic Regression: ")
-#		print("\tf1_score: {0}".format(LR_f1[category]))
-#		print("\tAccuracy: {0}".format(LR_accuracy[category]))
-#
-#		print("\n2. SVM: ")
-#		print("\tf1_score: {0}".format(SVM_f1[category]))
-#		print("\tAccuracy: {0}".format(SVM_accuracy[category]))
-#
-#		print("\n3. KNN: ")
-#		print("\tf1_score: {0}".format(KNN_f1[category]))
-#		print("\tAccuracy: {0}".format(KNN_accuracy[category]))
-#		print("\tBest K: {0}".format(KNN_best_Ks[category]))
-
-		print("\n4. ENS: ")
+		print("\n1. Logistic Regression: ")
+		print("\tf1_score: {0}".format(LR_f1[category]))
+		print("\tAccuracy: {0}".format(LR_accuracy[category]))
+		
+		print("\n2. SVM: ")
+		print("\tf1_score: {0}".format(SVM_f1[category]))
+		print("\tAccuracy: {0}".format(SVM_accuracy[category]))
+		
+		print("\n3. KNN: ")
+		print("\tf1_score: {0}".format(KNN_f1[category]))
+		print("\tAccuracy: {0}".format(KNN_accuracy[category]))
+		print("\tBest K: {0}".format(KNN_best_Ks[category]))
+		
+		print("\n4. AdaBoost Classifier: ")
+		print("\tf1_score: {0}".format(ABC_f1[category]))
+		print("\tAccuracy: {0}".format(ABC_accuracy[category]))
+		
+		print("\n5. ENS: ")
 		print("\tf1_score: {0}".format(ENS_f1[category]))
 		print("\tAccuracy: {0}".format(ENS_accuracy[category]))
-
+		
 		sys.stdout.flush()
-
+	
